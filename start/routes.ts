824d9d5b43031b59266f6c7d7275bcf2050b4ca5
Suzoka <mor9an77@gmail.com>
2025-05-15 11:16:15 +0200

    refacto routes

--- start/routes.ts
+++ start/routes.ts
@@ -12,16 +12,24 @@ import { middleware } from '#start/kernel'
 
 router.get('/', '#controllers/homeController.home').as('home')
 
-router.get('/login', '#controllers/authController.showLoginForm').as('auth.login').use(middleware.guest())
-router.post('/login', '#controllers/authController.login').use(middleware.guest())
+router.group(() => {
+  router.get('/login', '#controllers/authController.showLoginForm').as('auth.login')
+  router.post('/login', '#controllers/authController.login')
+  router.get('/register', '#controllers/authController.showRegisterForm').as('auth.register')
+  router.post('/register', '#controllers/authController.register')
+}).use(middleware.guest())
+
 router.post('/logout', '#controllers/authController.logout').as('auth.logout')
-router.get('/register', '#controllers/authController.showRegisterForm').as('auth.register').use(middleware.guest())
-router.post('/register', '#controllers/authController.register').use(middleware.guest())
+
+
+router.group(() => {
+  router.get('/advices/new', '#controllers/adviceController.new').as('advices.new')
+  router.put('/advices', '#controllers/adviceController.create')
+  router.get('/advices/:slug/edit', '#controllers/adviceController.edit').as('advices.edit')
+  router.put('/advices/:id', '#controllers/adviceController.update').as('advices.update')
+  router.delete('/advices/:id', '#controllers/adviceController.destroy').as('advices.destroy')
+}).use([middleware.auth(), middleware.admin()])
 
 router.get('/advices', '#controllers/adviceController.index').as('advices.index')
-router.get('/advices/new', '#controllers/adviceController.new').use(middleware.auth()).use(middleware.admin()).as('advices.new')
-router.put('/advices', '#controllers/adviceController.create').use(middleware.auth()).use(middleware.admin())
 router.get('/advices/:slug', '#controllers/adviceController.show').as('advices.show')
-router.get('/advices/:slug/edit', '#controllers/adviceController.edit').use(middleware.auth()).use(middleware.admin()).as('advices.edit')
-router.put('/advices/:id', '#controllers/adviceController.update').use(middleware.auth()).use(middleware.admin()).as('advices.update')
-router.delete('/advices/:id', '#controllers/adviceController.destroy').use(middleware.auth()).use(middleware.admin()).as('advices.destroy')
+
