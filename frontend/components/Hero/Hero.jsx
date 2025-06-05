fb7cb6da98d8b124ecbf15c4dee51911c6731bd4
Robin Vigier / B!nro <rob.vigier@gmail.com>
2025-06-05 22:42:09 +0200

    💄 UI and variant

--- frontend/components/Hero/Hero.jsx
+++ frontend/components/Hero/Hero.jsx
@@ -1,10 +1,10 @@
 import Heading from "../Titles/Titles";
 import styles from "./Hero.module.scss";
 
-const Hero= ({title, subtitle, children}) => {
+const Hero= ({title, subtitle, children, grey}) => {
 
     return (
-        <section className={styles.hero}>
+        <section className={`${styles.hero} ${grey ? styles.grey : ''}`}>
             <div className={styles.title_wrapper}>
                 <Heading as="h1" className={styles.title}>{title || "Title page"}</Heading>
                 {subtitle && <p className="subtitle">{subtitle}</p>}
